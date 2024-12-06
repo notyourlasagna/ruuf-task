64410ac69f4acd686280f4b7c1f691d1f59170b9
noutyourlasagna <matiasvasquezplass@gmail.com>
2024-12-06 19:13:13 -0300

    name_fix

--- MaxPanelsFitTask.py
+++ MaxPanelsFitTask.py
@@ -1,9 +1,9 @@
-def max_panels_fit(panel_width, panel_height, roof_width, roof_height):
+def max_panels_fit(panel_dimension_1, panel_dimension_2, roof_width, roof_height):
 
-    if panel_width <= 0 or panel_height <= 0 or roof_width <= 0 or roof_height <= 0:
+    if panel_dimension_1 <= 0 or panel_dimension_2 <= 0 or roof_width <= 0 or roof_height <= 0:
         raise ValueError("Las dimensiones deben ser positivas.\n" +
-                         f"Dimensiones recibidas: panel_width={panel_width}, panel_height={panel_height}, " +
-                         f"roof_width={roof_width}, roof_height={roof_height}")
+                         f"Dimensiones recibidas: panel_dimension_1={panel_dimension_1}," + 
+                         f"panel_dimension_2={panel_dimension_2}, roof_width={roof_width}, roof_height={roof_height}")
     
     def calculate_panels(panel_width, panel_height):
         panels_wide = roof_width // panel_width
@@ -18,13 +18,13 @@ def max_panels_fit(panel_width, panel_height, roof_width, roof_height):
         
         return total_full_panels + max(extra_width, extra_height)
 
-    panels_orientation_1 = calculate_panels(panel_width, panel_height)
-    panels_orientation_2 = calculate_panels(panel_height, panel_width)
+    panels_orientation_1 = calculate_panels(panel_dimension_1, panel_dimension_2)
+    panels_orientation_2 = calculate_panels(panel_dimension_2, panel_dimension_1)
     
     return int(max(panels_orientation_1, panels_orientation_2))
 
 
 try:
-    print(max_panels_fit(1, 2, 0, 5))
+    print(max_panels_fit(1, 2, 2, 4))
 except ValueError as e:
     print(e)
